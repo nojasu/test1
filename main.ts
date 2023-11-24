384469a0bd45e220cce4edc869ba7cae164a014c
nojasu <148558407+nojasu@users.noreply.github.com>
2023-11-24 15:33:16 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 forever(function () {
-    if (true) {
-    	
+    if (iscmons.button_is_pressed(pins.P1)) {
+        iscmons.led_turn(pins.P0, true)
+        while (iscmons.button_is_not_pressed(pins.P1)) {
+            iscmons.led_turn(pins.P0, true)
+        }
     }
 })
